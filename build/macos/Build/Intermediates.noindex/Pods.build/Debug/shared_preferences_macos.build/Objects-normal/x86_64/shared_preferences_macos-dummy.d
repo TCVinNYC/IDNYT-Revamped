c2e17113d58136b1179d1066d40c5500e32dd2dd
Stevenson Chittumuri <mchittum123@gmail.com>
2023-03-06 16:43:15 -0500

    added gitignore and simplified login_page

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/Objects-normal/x86_64/shared_preferences_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/prince/development/ora/macos/Pods/Target\ Support\ Files/shared_preferences_macos/shared_preferences_macos-dummy.m \
-  /Users/prince/development/ora/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/module.modulemap \
-  /Users/prince/development/ora/macos/Pods/Target\ Support\ Files/shared_preferences_macos/shared_preferences_macos-prefix.pch
