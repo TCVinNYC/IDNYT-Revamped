dde5e37e700b70b03d8413301e98679cef0aaa6e
Stevenson Chittumuri <mchittum123@gmail.com>
2023-04-17 23:31:03 -0400

    shortened name and added fade animation when holding on attendance item

--- lib/modules/home/views/professor_view_course_page.dart
+++ lib/modules/home/views/professor_view_course_page.dart
@@ -4,12 +4,9 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:idnyt_revamped/modules/home/views/create_course_page.dart';
 import 'package:idnyt_revamped/modules/home/widgets/professor_attendance_list_item_widget.dart';
-import 'package:idnyt_revamped/modules/home/widgets/professor_class_widget.dart';
 import 'package:idnyt_revamped/modules/models/student_attendance.model.dart';
 import 'package:idnyt_revamped/shared/models/course.model.dart';
-import 'package:idnyt_revamped/shared/models/user.model.dart';
 import 'package:idnyt_revamped/shared/providers/firebase.provider.dart';
 
 @RoutePage(name: 'ProfessorViewCoursePage')
@@ -111,14 +108,17 @@ class ProfessorViewCoursePage extends HookConsumerWidget {
               ),
             ),
             const SizedBox(height: 16.0),
-            const Text(
-              'Previous Attendances',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+            const Padding(
+              padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
+              child: Text(
+                'Previous Attendances',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            const SizedBox(height: 16.0),
+            const SizedBox(height: 10.0),
             Expanded(
               child: SafeArea(
                 child: StreamBuilder<QuerySnapshot<Object?>>(
@@ -158,7 +158,6 @@ class ProfessorViewCoursePage extends HookConsumerWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
           ],
         ),
       ),
--- lib/modules/home/widgets/professor_attendance_list_item_widget.dart
+++ lib/modules/home/widgets/professor_attendance_list_item_widget.dart
@@ -4,6 +4,8 @@ import 'package:idnyt_revamped/modules/models/student_attendance.model.dart';
 // ignore: depend_on_referenced_packages
 import 'package:intl/intl.dart';
 
+final showTimeProvider = StateProvider<bool>((ref) => false);
+
 class AttendanceListItemWidget extends ConsumerWidget {
   final int totalNumberStudent;
   final String date;
@@ -23,76 +25,87 @@ class AttendanceListItemWidget extends ConsumerWidget {
     DateFormat outputFormat = DateFormat('MMMM d, yyyy');
     String formattedDate = outputFormat.format(parsedDate);
 
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              formattedDate,
-              style:
-                  const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16.0),
-            Wrap(
-              spacing: 8.0,
-              runSpacing: 8.0,
-              children: List.generate(
-                students.length,
-                (index) {
-                  return Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 28.0,
-                        backgroundImage:
-                            NetworkImage(students[index].profilePicture),
-                      ),
-                      const SizedBox(height: 8.0),
-                      Text(students[index].name),
-                      // const SizedBox(height: 4.0),
-                      // Text(
-                      //   students[index].time,
-                      //   style: TextStyle(fontSize: 12.0),
-                      // ),
-                    ],
-                  );
-                },
+    bool showTime = ref.watch(showTimeProvider);
+
+    return InkWell(
+      enableFeedback: true,
+      onLongPress: () {
+        ref.read(showTimeProvider.notifier).state = !showTime;
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                formattedDate,
+                style: const TextStyle(
+                    fontSize: 16.0, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Present: ',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    Text(
-                      '${students.length}',
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
-                  ],
+              const SizedBox(height: 16.0),
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 8.0,
+                children: List.generate(
+                  students.length,
+                  (index) {
+                    List<String> nameParts = students[index].name.split(' ');
+                    return Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 28.0,
+                          backgroundImage:
+                              NetworkImage(students[index].profilePicture),
+                        ),
+                        const SizedBox(height: 8.0),
+                        AnimatedCrossFade(
+                            firstChild:
+                                Text('${nameParts[0]} ${nameParts[1][0]}'),
+                            secondChild: Text(students[index].time),
+                            crossFadeState: showTime
+                                ? CrossFadeState.showFirst
+                                : CrossFadeState.showSecond,
+                            duration: const Duration(milliseconds: 300)),
+                      ],
+                    );
+                  },
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Absent: ',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    Text(
-                      '${totalNumberStudent - students.length}',
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Present: ',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      Text(
+                        '${students.length}',
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Absent: ',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      Text(
+                        '${totalNumberStudent - students.length}',
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
