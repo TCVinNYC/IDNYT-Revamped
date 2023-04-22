3daf881c36fe7ec17b93d316804a68f528624e99
Stevenson Chittumuri <mchittum123@gmail.com>
2023-04-22 11:42:19 -0400

    basic ID page

--- lib/modules/home/views/id_card_page.dart
+++ lib/modules/home/views/id_card_page.dart
@@ -1,6 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:barcode/barcode.dart';
+
+final studentEmailProvider = Provider<String>((ref) => 'johndoe@my.nyit.edu');
+final currentSemesterProvider = Provider<String>((ref) => 'Spring 2023');
+final idNumberProvider = Provider<String>((ref) => '123456789');
+final creditAmountProvider = Provider<String>((ref) => '234.00');
+final profilePictureProvider =
+    Provider<String>((ref) => 'https://i.pravatar.cc/150?img=13');
+final studentNameProvider = Provider<String>((ref) => 'John Doe');
 
 @RoutePage(name: 'IDCardPage')
 class IDCardPage extends HookConsumerWidget {
@@ -8,23 +17,184 @@ class IDCardPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final studentEmail = ref.watch(studentEmailProvider);
+    final currentSemester = ref.watch(currentSemesterProvider);
+    final creditAmount = ref.watch(creditAmountProvider);
+    final idNumber = ref.watch(idNumberProvider);
+    final profilePicture = ref.watch(profilePictureProvider);
+    final studentName = ref.watch(studentNameProvider);
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        centerTitle: false,
+        backgroundColor: const Color(0xFF233972),
+        title: const Text(
+          'My NYIT ID',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
       ),
-      body: Center(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text('Hey this is the id card page!'),
-              Container(
-                color: Colors.green,
-                width: 100,
-                height: 100,
-              )
-            ],
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        decoration: const BoxDecoration(
+          color: Color(0xFF233972),
+          // borderRadius: BorderRadius.only(
+          //   bottomLeft: Radius.circular(32),
+          //   bottomRight: Radius.circular(32),
+          // ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 24,
+            ),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(profilePicture),
+                  radius: 64,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  studentName,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Divider(height: 0),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'NYIT Email',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      studentEmail,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                const Divider(height: 0),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'ID Number',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      idNumber,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                const Divider(height: 0),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Credit Balance',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "\$$creditAmount",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                const Divider(height: 0),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Current Semester',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      currentSemester,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.network(
+                          'https://www.qrstuff.com/images/sample.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 4, horizontal: 16),
+                          color: Colors.white,
+                          child: Text(
+                            studentEmail,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
