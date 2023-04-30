1a1ea08035fdbe0d273ce9439f96e7ec7f6378f7
Stevenson Chittumuri <mchittum123@gmail.com>
2023-04-30 18:47:27 -0400

    fix login page for all device sizes

--- lib/modules/login/views/error_page.dart
+++ lib/modules/login/views/error_page.dart
@@ -1,13 +1,9 @@
-/*
-If person that's logged in is not an @nyit.edu account
-Show them and error screen and explain the issue
-Of course sign them out and delete their account
-Then go back to login screen
- */
+import 'dart:ui';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:idnyt_revamped/routing/app_router.gr.dart';
 import 'package:idnyt_revamped/shared/providers/auth.provider.dart';
 import 'package:idnyt_revamped/shared/widgets/regular_button_widget.dart';
 
@@ -17,23 +13,34 @@ class ErrorPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final auth = ref.watch(authServiceProvider);
+    final auth = ref.read(authServiceProvider);
+    auth.deleteUser();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
       ),
       body: Center(
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text('Hey something is wrong with your account!'),
-              RegularButtonWidget(
-                text: "Return to Login Page",
-                onPressed: () => auth.deleteUser(),
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "You aren't an NYIT user.\nPlease sign in with an @NYIT email or contact your administrator!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                RegularButtonWidget(
+                    text: "Return to Login Page",
+                    onPressed: () {
+                      AutoRouter.of(context).replace(LoginPage());
+                    })
+              ],
+            ),
           ),
         ),
       ),
--- lib/modules/login/views/login_page.dart
+++ lib/modules/login/views/login_page.dart
@@ -1,10 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:idnyt_revamped/modules/login/models/lottie_with_text_model.dart';
 import 'package:idnyt_revamped/routing/app_router.gr.dart';
 import 'package:idnyt_revamped/shared/providers/firebase.provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:idnyt_revamped/modules/login/providers/lottie.provider.dart';
 import 'package:idnyt_revamped/shared/providers/auth.provider.dart';
 import 'package:idnyt_revamped/shared/widgets/regular_button_widget.dart';
 
@@ -14,71 +15,98 @@ class LoginPage extends HookConsumerWidget {
 
   final _pageController = PageController(viewportFraction: 1, initialPage: 0);
 
+  final lottieImageList = [
+    'assets/lottie/girl-studying-on-laptop.json',
+    'assets/lottie/calendar.json',
+    'assets/lottie/tap-nfc-apple.json',
+  ];
+
+  final lottieTitleList = [
+    'lottie_title_one'.tr(),
+    'lottie_title_two'.tr(),
+    'lottie_title_three'.tr(),
+  ];
+
+  final lottieSubtitleList = [
+    'lottie_subtitle_one'.tr(),
+    'lottie_subtitle_two'.tr(),
+    'lottie_subtitle_three'.tr(),
+  ];
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final lottieSignUpList = ref.read(lottieSignUpListProvider);
     final auth = ref.watch(authServiceProvider);
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width / 2,
-              padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
-              child: const Image(
-                image: AssetImage('assets/images/IDNYTColor.png'),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
+                child: const Image(
+                  image: AssetImage('assets/images/IDNYTColor.png'),
+                ),
               ),
-            ),
-            Expanded(
-              child: PageView.builder(
-                itemCount: lottieTitleList.length,
-                physics: const AlwaysScrollableScrollPhysics(),
-                pageSnapping: true,
-                controller: _pageController,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    child: lottieSignUpList[index],
-                    padding: const EdgeInsets.fromLTRB(5, 20, 5, 0),
-                  );
-                },
-                onPageChanged: (value) {
-                  ref.read(lottiePositionProvider.notifier).state = value;
-                },
+              Flexible(
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                    LottieTextModel(
+                      lottieLocation: lottieImageList[0],
+                      titleText: lottieTitleList[0],
+                      subtitleText: lottieSubtitleList[0],
+                    ),
+                    LottieTextModel(
+                      lottieLocation: lottieImageList[1],
+                      titleText: lottieTitleList[1],
+                      subtitleText: lottieSubtitleList[1],
+                    ),
+                    LottieTextModel(
+                      lottieLocation: lottieImageList[2],
+                      titleText: lottieTitleList[2],
+                      subtitleText: lottieSubtitleList[2],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-              child: SmoothPageIndicator(
-                controller: _pageController,
-                count: lottieSignUpList.length,
-                textDirection: TextDirection.ltr,
-                effect: const ExpandingDotsEffect(),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: SmoothPageIndicator(
+                  controller: _pageController,
+                  count: lottieImageList.length,
+                  effect: const ExpandingDotsEffect(),
+                ),
               ),
-            ),
-            RegularButtonWidget(
-                text: "Get Started",
-                onPressed: () async {
-                  await auth.signInWithGoogle();
-                  if (auth.currentUser != null) {
-                    debugPrint('Auth is vaild for ${auth.currentUser?.email}');
-                    if (auth.currentUser!.email!.endsWith("@nyit.edu")) {
+              RegularButtonWidget(
+                  text: "Get Started",
+                  onPressed: () async {
+                    await auth.signInWithGoogle();
+                    if (auth.currentUser != null) {
                       debugPrint(
-                          'Auth ends in @NYIT.edu :D\nGoing to Tab Navigation Page');
-                      await ref.read(firestoreProvider).checkUserData();
-                      // ignore: use_build_context_synchronously
-                      AutoRouter.of(context).replace(const TabControllerPage());
-                    } else {
-                      debugPrint(
-                          'Error during auth for ${auth.currentUser!.email}');
-                      // ignore: use_build_context_synchronously
-                      AutoRouter.of(context).push(const ErrorPage());
+                          'Auth is vaild for ${auth.currentUser?.email}');
+                      if (auth.currentUser!.email!.endsWith("@nyit.edu")) {
+                        debugPrint(
+                            'Auth ends in @NYIT.edu :D\nGoing to Tab Navigation Page');
+                        await ref.read(firestoreProvider).checkUserData();
+                        // ignore: use_build_context_synchronously
+                        AutoRouter.of(context)
+                            .replace(const TabControllerPage());
+                      } else {
+                        debugPrint(
+                            'Error during auth for ${auth.currentUser!.email}');
+                        // ignore: use_build_context_synchronously
+                        AutoRouter.of(context).push(const ErrorPage());
+                      }
                     }
-                  }
-                })
-          ],
+                  })
+            ],
+          ),
         ),
       ),
     );
