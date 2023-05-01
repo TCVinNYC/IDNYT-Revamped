474f4df952b509710f38a3949d5be68f3fa514ec
Pemba Rinzi Sherpa <pemba_sherpa64@yahoo.com>
2023-05-01 11:15:32 -0400

    Student Courses (#10)
    I have added your previous ios folder. But the problem is I'm unable to
    do any testing.
    
    ---------
    
    Co-authored-by: Stevenson Chittumuri <stevensonchittumuri@gmail.com>

---
+++ lib/modules/home/views/student_course_detail_page.dart
@@ -0,0 +1,88 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:idnyt_revamped/routing/app_router.gr.dart';
+import 'package:idnyt_revamped/shared/widgets/regular_button_widget.dart';
+
+@RoutePage(name: 'StudentCourseDetailPage')
+class StudentCourseDetailPage extends StatelessWidget {
+  final String courseName;
+  final String syllabus;
+  final String professorName;
+  final String classTime;
+  final String classLocation;
+  final String classDay;
+
+  const StudentCourseDetailPage({
+    Key? key,
+    required this.courseName,
+    required this.syllabus,
+    required this.professorName,
+    required this.classTime,
+    required this.classLocation,
+    required this.classDay,
+  }) : super(key: key);
+
+  void _attendClass() {
+    debugPrint("Trying to attend class");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(courseName),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Course Details and Timing:',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 8.0),
+                Text('Professor: $professorName'),
+                const SizedBox(height: 8.0),
+                Text('Class Time: $classTime'),
+                const SizedBox(height: 8.0),
+                Text('Class Location: $classLocation'),
+                const SizedBox(height: 8.0),
+                Text('Class Day: $classDay'),
+                const SizedBox(height: 16.0),
+                Text(
+                  'Course Syllabus:',
+                  textAlign: TextAlign.justify,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                // SizedBox(height: 8.0),
+                Text(syllabus, style: Theme.of(context).textTheme.bodyMedium),
+                const SizedBox(height: 16.0),
+                // Center(
+                //   child: TextButton(
+                //     onPressed: () {
+                //       // TODO: Implement attend button tap action.
+                //       // This code is commented out for now.
+                //       //print('Attend button tapped.');
+                //     },
+                //     child: Text('Attend'),
+                //   ),
+                // ),
+                Text("Class Attendance 80%",
+                    style: Theme.of(context).textTheme.bodyMedium),
+                const LinearProgressIndicator(
+                  value: 0.8, // Percentage of class attended
+                  color: Colors.green,
+                  semanticsLabel: 'Linear progress indicator',
+                ),
+
+                const SizedBox(height: 32.0),
+                RegularButtonWidget(text: "Attend", onPressed: _attendClass)
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/routing/app_router.gr.dart
+++ lib/routing/app_router.gr.dart
@@ -11,24 +11,26 @@
 import 'package:auto_route/auto_route.dart' as _i16;
 import 'package:cloud_firestore/cloud_firestore.dart' as _i18;
 import 'package:flutter/material.dart' as _i17;
-import 'package:idnyt_revamped/modules/home/views/admin_home_page.dart' as _i6;
+import 'package:idnyt_revamped/modules/home/views/admin_home_page.dart' as _i7;
 import 'package:idnyt_revamped/modules/home/views/attendance_detail_page.dart'
-    as _i5;
+    as _i6;
 import 'package:idnyt_revamped/modules/home/views/course_messaging_page.dart'
-    as _i8;
-import 'package:idnyt_revamped/modules/home/views/create_course_page.dart'
     as _i9;
-import 'package:idnyt_revamped/modules/home/views/id_card_page.dart' as _i10;
-import 'package:idnyt_revamped/modules/home/views/professor_home_page.dart'
+import 'package:idnyt_revamped/modules/home/views/create_course_page.dart'
     as _i11;
+import 'package:idnyt_revamped/modules/home/views/enter_id_page.dart' as _i10;
+import 'package:idnyt_revamped/modules/home/views/id_card_page.dart' as _i12;
+import 'package:idnyt_revamped/modules/home/views/professor_home_page.dart'
+    as _i13;
 import 'package:idnyt_revamped/modules/home/views/professor_view_course_page.dart'
-    as _i7;
+    as _i8;
+import 'package:idnyt_revamped/modules/home/views/student_course_detail_page.dart'
+    as _i5;
 import 'package:idnyt_revamped/modules/home/views/student_home_page.dart'
     as _i4;
-import 'package:idnyt_revamped/modules/login/views/error_page.dart' as _i13;
-import 'package:idnyt_revamped/modules/login/views/login_page.dart' as _i14;
+import 'package:idnyt_revamped/modules/login/views/error_page.dart' as _i14;
+import 'package:idnyt_revamped/modules/login/views/login_page.dart' as _i15;
 import 'package:idnyt_revamped/shared/models/user.model.dart' as _i19;
-import 'package:idnyt_revamped/modules/home/views/enter_id_page.dart' as _i15;
 import 'package:idnyt_revamped/shared/views/splash_screen.dart' as _i3;
 import 'package:idnyt_revamped/shared/views/tab_controller_page.dart' as _i2;
 import 'package:idnyt_revamped/tests/extra_page.dart' as _i1;
@@ -62,11 +64,26 @@ abstract class $AppRouter extends _i16.RootStackRouter {
         child: const _i4.StudentHomePage(),
       );
     },
+    StudentCourseDetailPage.name: (routeData) {
+      final args = routeData.argsAs<StudentCourseDetailPageArgs>();
+      return _i16.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: _i5.StudentCourseDetailPage(
+          key: args.key,
+          courseName: args.courseName,
+          syllabus: args.syllabus,
+          professorName: args.professorName,
+          classTime: args.classTime,
+          classLocation: args.classLocation,
+          classDay: args.classDay,
+        ),
+      );
+    },
     AttendanceDetailPage.name: (routeData) {
       final args = routeData.argsAs<AttendanceDetailPageArgs>();
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i5.AttendanceDetailPage(
+        child: _i6.AttendanceDetailPage(
           key: args.key,
           documentSnapshot: args.documentSnapshot,
         ),
@@ -76,7 +93,7 @@ abstract class $AppRouter extends _i16.RootStackRouter {
       final args = routeData.argsAs<AdminHomePageArgs>();
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i6.AdminHomePage(
+        child: _i7.AdminHomePage(
           key: args.key,
           userData: args.userData,
         ),
@@ -86,7 +103,7 @@ abstract class $AppRouter extends _i16.RootStackRouter {
       final args = routeData.argsAs<ProfessorViewCoursePageArgs>();
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i7.ProfessorViewCoursePage(
+        child: _i8.ProfessorViewCoursePage(
           key: args.key,
           documentSnapshot: args.documentSnapshot,
         ),
@@ -95,20 +112,30 @@ abstract class $AppRouter extends _i16.RootStackRouter {
     CourseMessagingPage.name: (routeData) {
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i8.CourseMessagingPage(),
+        child: const _i9.CourseMessagingPage(),
+      );
+    },
+    EnterIDPage.name: (routeData) {
+      final args = routeData.argsAs<EnterIDPageArgs>();
+      return _i16.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: _i10.EnterIDPage(
+          key: args.key,
+          userData: args.userData,
+        ),
       );
     },
     CreateCoursePage.name: (routeData) {
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i9.CreateCoursePage(),
+        child: const _i11.CreateCoursePage(),
       );
     },
     IDCardPage.name: (routeData) {
       final args = routeData.argsAs<IDCardPageArgs>();
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i10.IDCardPage(
+        child: _i12.IDCardPage(
           key: args.key,
           userData: args.userData,
         ),
@@ -118,7 +145,7 @@ abstract class $AppRouter extends _i16.RootStackRouter {
       final args = routeData.argsAs<ProfessorHomePageArgs>();
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i11.ProfessorHomePage(
+        child: _i13.ProfessorHomePage(
           key: args.key,
           userData: args.userData,
         ),
@@ -127,7 +154,7 @@ abstract class $AppRouter extends _i16.RootStackRouter {
     ErrorPage.name: (routeData) {
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i13.ErrorPage(),
+        child: const _i14.ErrorPage(),
       );
     },
     LoginPage.name: (routeData) {
@@ -135,17 +162,7 @@ abstract class $AppRouter extends _i16.RootStackRouter {
           routeData.argsAs<LoginPageArgs>(orElse: () => const LoginPageArgs());
       return _i16.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: _i14.LoginPage(key: args.key),
-      );
-    },
-    EnterIDPage.name: (routeData) {
-      final args = routeData.argsAs<EnterIDPageArgs>();
-      return _i16.AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: _i15.EnterIDPage(
-          key: args.key,
-          userData: args.userData,
-        ),
+        child: _i15.LoginPage(key: args.key),
       );
     },
   };
@@ -208,7 +225,71 @@ class StudentHomePage extends _i16.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i5.AttendanceDetailPage]
+/// [_i5.StudentCourseDetailPage]
+class StudentCourseDetailPage
+    extends _i16.PageRouteInfo<StudentCourseDetailPageArgs> {
+  StudentCourseDetailPage({
+    _i17.Key? key,
+    required String courseName,
+    required String syllabus,
+    required String professorName,
+    required String classTime,
+    required String classLocation,
+    required String classDay,
+    List<_i16.PageRouteInfo>? children,
+  }) : super(
+          StudentCourseDetailPage.name,
+          args: StudentCourseDetailPageArgs(
+            key: key,
+            courseName: courseName,
+            syllabus: syllabus,
+            professorName: professorName,
+            classTime: classTime,
+            classLocation: classLocation,
+            classDay: classDay,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'StudentCourseDetailPage';
+
+  static const _i16.PageInfo<StudentCourseDetailPageArgs> page =
+      _i16.PageInfo<StudentCourseDetailPageArgs>(name);
+}
+
+class StudentCourseDetailPageArgs {
+  const StudentCourseDetailPageArgs({
+    this.key,
+    required this.courseName,
+    required this.syllabus,
+    required this.professorName,
+    required this.classTime,
+    required this.classLocation,
+    required this.classDay,
+  });
+
+  final _i17.Key? key;
+
+  final String courseName;
+
+  final String syllabus;
+
+  final String professorName;
+
+  final String classTime;
+
+  final String classLocation;
+
+  final String classDay;
+
+  @override
+  String toString() {
+    return 'StudentCourseDetailPageArgs{key: $key, courseName: $courseName, syllabus: $syllabus, professorName: $professorName, classTime: $classTime, classLocation: $classLocation, classDay: $classDay}';
+  }
+}
+
+/// generated route for
+/// [_i6.AttendanceDetailPage]
 class AttendanceDetailPage
     extends _i16.PageRouteInfo<AttendanceDetailPageArgs> {
   AttendanceDetailPage({
@@ -247,7 +328,7 @@ class AttendanceDetailPageArgs {
 }
 
 /// generated route for
-/// [_i6.AdminHomePage]
+/// [_i7.AdminHomePage]
 class AdminHomePage extends _i16.PageRouteInfo<AdminHomePageArgs> {
   AdminHomePage({
     _i17.Key? key,
@@ -285,7 +366,7 @@ class AdminHomePageArgs {
 }
 
 /// generated route for
-/// [_i7.ProfessorViewCoursePage]
+/// [_i8.ProfessorViewCoursePage]
 class ProfessorViewCoursePage
     extends _i16.PageRouteInfo<ProfessorViewCoursePageArgs> {
   ProfessorViewCoursePage({
@@ -324,7 +405,7 @@ class ProfessorViewCoursePageArgs {
 }
 
 /// generated route for
-/// [_i8.CourseMessagingPage]
+/// [_i9.CourseMessagingPage]
 class CourseMessagingPage extends _i16.PageRouteInfo<void> {
   const CourseMessagingPage({List<_i16.PageRouteInfo>? children})
       : super(
@@ -338,7 +419,45 @@ class CourseMessagingPage extends _i16.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i9.CreateCoursePage]
+/// [_i10.EnterIDPage]
+class EnterIDPage extends _i16.PageRouteInfo<EnterIDPageArgs> {
+  EnterIDPage({
+    _i17.Key? key,
+    required _i19.UserModel userData,
+    List<_i16.PageRouteInfo>? children,
+  }) : super(
+          EnterIDPage.name,
+          args: EnterIDPageArgs(
+            key: key,
+            userData: userData,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'EnterIDPage';
+
+  static const _i16.PageInfo<EnterIDPageArgs> page =
+      _i16.PageInfo<EnterIDPageArgs>(name);
+}
+
+class EnterIDPageArgs {
+  const EnterIDPageArgs({
+    this.key,
+    required this.userData,
+  });
+
+  final _i17.Key? key;
+
+  final _i19.UserModel userData;
+
+  @override
+  String toString() {
+    return 'EnterIDPageArgs{key: $key, userData: $userData}';
+  }
+}
+
+/// generated route for
+/// [_i11.CreateCoursePage]
 class CreateCoursePage extends _i16.PageRouteInfo<void> {
   const CreateCoursePage({List<_i16.PageRouteInfo>? children})
       : super(
@@ -352,7 +471,7 @@ class CreateCoursePage extends _i16.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i10.IDCardPage]
+/// [_i12.IDCardPage]
 class IDCardPage extends _i16.PageRouteInfo<IDCardPageArgs> {
   IDCardPage({
     _i17.Key? key,
@@ -390,7 +509,7 @@ class IDCardPageArgs {
 }
 
 /// generated route for
-/// [_i11.ProfessorHomePage]
+/// [_i13.ProfessorHomePage]
 class ProfessorHomePage extends _i16.PageRouteInfo<ProfessorHomePageArgs> {
   ProfessorHomePage({
     _i17.Key? key,
@@ -428,45 +547,7 @@ class ProfessorHomePageArgs {
 }
 
 /// generated route for
-/// [_i12.ProfilePage]
-class ProfilePage extends _i16.PageRouteInfo<ProfilePageArgs> {
-  ProfilePage({
-    _i17.Key? key,
-    required _i19.UserModel userData,
-    List<_i16.PageRouteInfo>? children,
-  }) : super(
-          ProfilePage.name,
-          args: ProfilePageArgs(
-            key: key,
-            userData: userData,
-          ),
-          initialChildren: children,
-        );
-
-  static const String name = 'ProfilePage';
-
-  static const _i16.PageInfo<ProfilePageArgs> page =
-      _i16.PageInfo<ProfilePageArgs>(name);
-}
-
-class ProfilePageArgs {
-  const ProfilePageArgs({
-    this.key,
-    required this.userData,
-  });
-
-  final _i17.Key? key;
-
-  final _i19.UserModel userData;
-
-  @override
-  String toString() {
-    return 'ProfilePageArgs{key: $key, userData: $userData}';
-  }
-}
-
-/// generated route for
-/// [_i13.ErrorPage]
+/// [_i14.ErrorPage]
 class ErrorPage extends _i16.PageRouteInfo<void> {
   const ErrorPage({List<_i16.PageRouteInfo>? children})
       : super(
@@ -480,7 +561,7 @@ class ErrorPage extends _i16.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i14.LoginPage]
+/// [_i15.LoginPage]
 class LoginPage extends _i16.PageRouteInfo<LoginPageArgs> {
   LoginPage({
     _i17.Key? key,
@@ -505,43 +586,5 @@ class LoginPageArgs {
   @override
   String toString() {
     return 'LoginPageArgs{key: $key}';
-  }
-}
-
-/// generated route for
-/// [_i15.EnterIDPage]
-class EnterIDPage extends _i16.PageRouteInfo<EnterIDPageArgs> {
-  EnterIDPage({
-    _i17.Key? key,
-    required _i19.UserModel userData,
-    List<_i16.PageRouteInfo>? children,
-  }) : super(
-          EnterIDPage.name,
-          args: EnterIDPageArgs(
-            key: key,
-            userData: userData,
-          ),
-          initialChildren: children,
-        );
-
-  static const String name = 'EnterIDPage';
-
-  static const _i16.PageInfo<EnterIDPageArgs> page =
-      _i16.PageInfo<EnterIDPageArgs>(name);
-}
-
-class EnterIDPageArgs {
-  const EnterIDPageArgs({
-    this.key,
-    required this.userData,
-  });
-
-  final _i17.Key? key;
-
-  final _i19.UserModel userData;
-
-  @override
-  String toString() {
-    return 'EnterIDPageArgs{key: $key, userData: $userData}';
   }
 }
